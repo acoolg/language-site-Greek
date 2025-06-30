ca1a13fed7d0bacd8e4a8d2ee24a35d55f0562a4
acoolg <vecy11332@gmail.com>
2025-06-30 12:38:29 +0800

    a

--- editor.js
+++ editor.js
@@ -1,1 +1,24 @@
-var localstor = window.localStorage()
+var request = indexedDB.open("myDatabase", 1);
+var db = undefined
+
+request.onerror = function (event) {
+    console.error("IndexedDB error:", event.target.error);
+};
+
+request.onsuccess = function (event) {
+    db = request.result;
+    console.log(request.result)
+};
+
+
+
+request.onupgradeneeded = function (event) {
+    var db = event.target.result;
+    var objectStore = db.createObjectStore("myObjectStore", { keyPath: "id" });
+};
+
+if (!window.indexedDB) {
+    window.alert(
+        "Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.",
+    );
+}
